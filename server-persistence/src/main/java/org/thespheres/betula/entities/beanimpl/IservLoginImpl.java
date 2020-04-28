9b09af11bfbf08c2bc87fcde05109a7ceb40abca
Boris Heithecker <boris.heithecker@gmx.net>
2020-04-28 16:46:53 +0200

    Remove Cert Login

--- server-persistence/src/main/java/org/thespheres/betula/entities/beanimpl/IservLoginImpl.java
+++ server-persistence/src/main/java/org/thespheres/betula/entities/beanimpl/IservLoginImpl.java
@@ -5,26 +5,15 @@
  */
 package org.thespheres.betula.entities.beanimpl;
 
-import java.util.NoSuchElementException;
 import javax.annotation.security.PermitAll;
 import javax.ejb.EJB;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
-import javax.inject.Inject;
-import javax.naming.InvalidNameException;
-import javax.naming.NamingException;
-import javax.naming.directory.Attribute;
-import javax.naming.ldap.LdapName;
-import javax.naming.ldap.Rdn;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.security.auth.login.LoginException;
 import org.thespheres.betula.document.Signee;
 import org.thespheres.betula.entities.SigneeEntity;
-import org.thespheres.betula.entities.config.ConfiguredModelException;
 import org.thespheres.betula.security.iservlogin.IservLogin;
-import org.thespheres.betula.services.AppPropertyNames;
-import org.thespheres.betula.services.LocalProperties;
 
 /**
  *
@@ -38,8 +27,6 @@ public class IservLoginImpl implements IservLogin {
 
     @PersistenceContext(unitName = "betula0")
     private EntityManager em;
-    @Inject
-    private LocalProperties lp;
 
     @Override
     public String[] getGroups(String prefix, String suffix) {
@@ -47,47 +34,6 @@ public class IservLoginImpl implements IservLogin {
         SigneeEntity se;
         if ((se = em.find(SigneeEntity.class, signee)) != null) {
             return se.getGroups();
-        }
-        return null;
-    }
-
-    @Override
-    public String[] getGroups(String ldapName) {
-        LdapName dName;
-        try {
-            dName = new LdapName(ldapName);
-        } catch (InvalidNameException ex) {
-            LoginException lex = new LoginException();
-            lex.initCause(ex);
-            return null;
-        }
-        String cn = null;
-        for (Rdn rdn : dName.getRdns()) {
-            Attribute attr;
-            if ((attr = rdn.toAttributes().get("CN")) != null) {
-                try {
-                    cn = (String) attr.get();
-                } catch (NamingException | NoSuchElementException | ClassCastException ex) {
-                }
-                break;
-            }
-        }
-        if (cn != null) {
-            String[] elements = cn.split("@");
-            String suffix = System.getenv(AppPropertyNames.ENV_SIGNEE_SUFFIX);
-            if (suffix == null) {//Legacy case
-                suffix = lp.getProperty(AppPropertyNames.LP_DEFAULT_SIGNEE_SUFFIX);
-            }
-            if (suffix == null) {
-                throw new ConfiguredModelException(AppPropertyNames.ENV_SIGNEE_SUFFIX);
-            }
-            if (elements.length == 2 && elements[1].equals(suffix)) {
-                Signee signee = new Signee(elements[0], suffix, true);
-                SigneeEntity se;
-                if ((se = em.find(SigneeEntity.class, signee)) != null) {
-                    return se.getGroups();
-                }
-            }
         }
         return null;
     }
