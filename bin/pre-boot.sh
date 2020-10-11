d2243d44573d0f7af6213c62af8c23d975c3a71b
Boris Heithecker <boris.heithecker@gmx.net>
2020-10-11 14:17:55 +0200

    Save p12-File in SECRETS_DIR

--- bin/pre-boot.sh
+++ bin/pre-boot.sh
@@ -16,7 +16,7 @@ if [ -f $APP_RESOURCES/host.conf ]; then
 fi
 
 SERVER_KEY=${SECRETS}/server.key
-SERVER_PKCS12_FILE=/tmp/server.p12
+SERVER_PKCS12_FILE=${SECRETS_DIR}/server.p12 //In futur, server.p12 should be provided in /run/secrets
 SERVER_CERT=${SECRETS}/server.crt
 
 set +x
