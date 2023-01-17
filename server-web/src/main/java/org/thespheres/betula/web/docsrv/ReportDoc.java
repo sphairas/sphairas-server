1fbf0392fae67de6e7226cde82f4e86f0007f951
Boris Heithecker <boris.heithecker@gmx.net>
2023-01-17 07:32:13 +0100

    Fixed NPE ZensurenschnittValidation

--- server-web/src/main/java/org/thespheres/betula/web/docsrv/ReportDoc.java
+++ server-web/src/main/java/org/thespheres/betula/web/docsrv/ReportDoc.java
@@ -7,6 +7,7 @@ package org.thespheres.betula.web.docsrv;
 
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 import org.thespheres.betula.TermId;
@@ -25,7 +26,7 @@ class ReportDoc extends AbstractReportDocument {
     private final Map<TermId, Map<Subject, Grade>> grades;
 
     ReportDoc(final DocumentId report, final TermId term, final Map<TermId, Map<Subject, Grade>> map, Marker[] m, LocalDate reportDate) {
-        super(report, term, map.get(term), reportDate);
+        super(report, term, map.containsKey(term) ? map.get(term) : Collections.EMPTY_MAP, reportDate);
         this.grades = map;
         Arrays.stream(m).forEach(markers::add);
     }
