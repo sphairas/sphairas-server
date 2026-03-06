a2481983864b99d2f9188d09f5b669b45bba3b21
Boris Heithecker <boris.heithecker@gmx.net>
2026-03-06 23:58:47 +0100

    linted

--- server-service/src/main/java/org/thespheres/betula/server/service/UnitsServiceResource.java
+++ server-service/src/main/java/org/thespheres/betula/server/service/UnitsServiceResource.java
@@ -2,8 +2,6 @@ package org.thespheres.betula.server.service;
 
 import jakarta.ejb.EJB;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.core.Context;
-import jakarta.ws.rs.core.UriInfo;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.Path;
@@ -11,7 +9,6 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.SecurityContext;
 import jakarta.ws.rs.core.StreamingOutput;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -25,14 +22,13 @@ import org.thespheres.betula.services.ws.BetulaWebService;
  */
 @Path("units")
 public class UnitsServiceResource {
-    
-    @Context
-    private UriInfo context;
-    @Context
-    private SecurityContext securityContext;
+
+//    @Context
+//    private UriInfo context;
+//    @Context
+//    private SecurityContext securityContext;
     @Inject
     private AppResources appResources;
-    
     @EJB(beanName = "BetulaAdminService")
     private BetulaWebService delegate;
 
@@ -40,21 +36,21 @@ public class UnitsServiceResource {
      * Retrieves representation of an instance of
      * org.thespheres.betula.web.rest.ContainerService
      *
-     * @param requestContainer
+     * @param in
      * @return an instance of java.lang.String
+     * @throws java.lang.Exception
      */
     @Path("solicit")
     @POST
     @Consumes(MediaType.APPLICATION_XML)
     @Produces(MediaType.APPLICATION_XML)
     public Response solicit(final InputStream in) throws Exception {
-        javax.xml.bind.JAXBContext jaxb = appResources.getJAXBContext();
+        final javax.xml.bind.JAXBContext jaxb = appResources.getJAXBContext();
 //        if (!securityContext.isUserInRole("unitadmin")) {
 //            throw new IllegalAccessException();
 //        }
-        System.out.println("solicit, user: " + securityContext.getUserPrincipal().getName() + ", in role unitadmin " + securityContext.isUserInRole("unitadmin"));
-        Container requestContainer = (Container) jaxb.createUnmarshaller().unmarshal(in);
-        Container returnContainer = delegate.solicit(requestContainer);
+        final Container requestContainer = (Container) jaxb.createUnmarshaller().unmarshal(in);
+        final Container returnContainer = delegate.solicit(requestContainer);
         final StreamingOutput stream = (OutputStream os) -> {
             try {
                 jaxb.createMarshaller().marshal(returnContainer, os);
@@ -66,5 +62,5 @@ public class UnitsServiceResource {
                 .entity(stream)
                 .build();
     }
-    
+
 }
