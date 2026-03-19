56509b42a5c410e7f2f06f2a6c6d42f26cd39269
Boris Heithecker <boris.heithecker@gmx.net>
2026-03-19 18:12:47 +0100

    extended internal client

--- sphairas-server-libs/src/main/java/org/thespheres/betula/server/beans/clients/ServiceInternalClient.java
+++ sphairas-server-libs/src/main/java/org/thespheres/betula/server/beans/clients/ServiceInternalClient.java
@@ -2,8 +2,13 @@ package org.thespheres.betula.server.beans.clients;
 
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import org.thespheres.betula.StudentId;
+import org.thespheres.betula.TermId;
 import org.thespheres.betula.UnitId;
+import org.thespheres.betula.assess.Grade;
 import org.thespheres.betula.document.DocumentId;
+import org.thespheres.betula.document.Marker;
+import org.thespheres.betula.document.Signee;
 
 //Annotation wird in Payara Server gebraucht, in Payara Micro angeblich nicht
 //@Dependent
@@ -22,6 +27,54 @@ public interface ServiceInternalClient {
     public String getUnitCommonName(
             @QueryParam("studentId") DocumentId cNames,
             @QueryParam("authority") UnitId unit);
+
+    @GET
+    @Path("signee-common-name")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getSigneeCommonName(
+            @QueryParam("signee") Signee signee);
+
+    @GET
+    @Path("signee-primary-unit")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getSigneePrimaryUnit(
+            @QueryParam("primary-unit-head-teachers-document") DocumentId klassenlehrerDoc,
+            @QueryParam("signee") Signee signee);
+
+    @GET
+    @Path("applicable-tickets")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String findApplicableTickets(
+            @QueryParam("target-document") DocumentId target,
+            @QueryParam("term") TermId term,
+            @QueryParam("student") StudentId student);
+
+    @GET
+    @Path("select")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String selectSingle(
+            @QueryParam("target") DocumentId target,
+            @QueryParam("student") StudentId student,
+            @QueryParam("term") TermId term);
+
+    @GET
+    @Path("submit-grade-value")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String submitSingleGradeValue(
+            @QueryParam("target") DocumentId target,
+            @QueryParam("student") StudentId student,
+            @QueryParam("term") TermId term,
+            @QueryParam("grade") Grade grade);
+
+    @GET
+    @Path("submit-text-value")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String submitSingleTextValue(
+            @QueryParam("target") DocumentId target,
+            @QueryParam("student") StudentId student,
+            @QueryParam("term") TermId term,
+            @QueryParam("section") Marker section,
+            @QueryParam("text") String text);
 
     @GET
     @Path("ping")
